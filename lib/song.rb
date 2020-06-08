2de9daadb6af6aadf0f85113e22c6cf575aaf199
Rachel S. Lee <rslee0025@gmail.com>
2020-06-08 20:59:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
     self.class.all << self
   end
 
-  def self.create(name)
-    song = self.new(name)
+  def self.create
+    song = self.new
     song.save
     return song.all
   end 
